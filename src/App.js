af3888a338831badecc22618f0712aed840cc523
unknown <jebaliseifeddine00@gmail.com>
2023-10-08 22:10:29 +0100

    secend commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { useState } from 'react';
 import Todolist from './components/Todolist';
 import './App.css';
 
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
 
-  const addTodo = () => {
+  const handleAddTodo= () => {
     if (newTodo.trim() !== '') {
       setTodos([...todos, { text: newTodo, completed: false }]);
       setNewTodo('');
     }
   };
+
+
 
   const deleteTodo = (index) => {
     const updatedTodos = [...todos];
@@ -30,13 +33,75 @@ function App() {
       <h1>ToDo List</h1>
       <input
         type="text"
-        placeholder="Ajouter une tâche..."
+        placeholder="Porsche carrera 911..."
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
       />
-      <button onClick={addTodo}>Ajouter</button>
+      <button onClick={handleAddTodo}>Ajouter</button>
+      <button onClick={handleAddTodo}>Supprimer</button>
+
+      <Todolist todos={todos} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
+   
+   
+   
+   
+   
+      <input
+        type="text"
+        placeholder="BMW e92 m3..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Ajouter</button>
+      <button onClick={handleAddTodo}>Supprimer</button>
+
+      <Todolist todos={todos} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
+   
+   
+   
+      <input
+        type="text"
+        placeholder="Porsche Macan gts..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Ajouter</button>
+      <button onClick={handleAddTodo}>Supprimer</button>
+
+      <Todolist todos={todos} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
+   
+   
+      <input
+        type="text"
+        placeholder="Bmw e60 m5..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Ajouter</button>
+      <button onClick={handleAddTodo}>Supprimer</button>
+
+      <Todolist todos={todos} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
+   
+   
+      <input
+        type="text"
+        placeholder="Ferrari Enzo..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Ajouter</button>
+      <button onClick={handleAddTodo}>Supprimer</button>
+
       <Todolist todos={todos} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
     </div>
+   
+
+    
+
+
+
+
+  
   );
 }
 
